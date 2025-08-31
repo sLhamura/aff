630581ec3669f7b11203a65236f92cc4642a085e
sLhamura <fikriazabusiness@gmail.com>
2025-08-31 15:28:38 +0700

    Create aff.js

---
+++ aff.js
@@ -0,0 +1,36 @@
+
+  const hashMap = {
+    "#datajumbo": "https://www.datajumbo.co/?via=danny",
+    "#betterpic": "https://www.betterpic.io?via=danny",
+  "#qrcodeveloper": "https://qrcodeveloper.com/?via=danny",
+    "#helloprenup": "https://helloprenup.com?via=danny",
+    "#tickernerd": "https://tickernerd.com/?via=danny-bayuardi",
+    "#notifier": "http://notifier.so?via=danny",
+    "#headshotpro": "https://www.headshotpro.com?via=danny-bayuardi",
+    "#pallyy": "https://pallyy.com/?via=danny",
+    "#repper": "https://repper.app?via=danny",
+    "#agenticflow": "https://agenticflow.ai/?via=danny",
+    "#heygen": "https://www.heygen.com/?sid=rewardful&via=danny-bayuardi",
+    "#goheather": "https://www.goheather.io/?via=danny",
+    "#contactbutton": "https://contactbutton.com?via=danny",
+    "#keygen": "https://keygen.sh?via=danny",
+    "#omakase": "https://www.omakase.ai/?via=danny",
+    "#boldsign": "https://boldsign.com?via=danny",
+    "#aiproductshot": "https://aiproductshot.com?via=danny",
+    "#wpsoffice":"https://go.wps.com/a8w6hRJJA4T6?fpr=j9ecoi",
+    "#foodshot": "https://foodshot.ai/?via=danny"
+    // add more here...
+  };
+
+  function checkHash() {
+    const targetUrl = hashMap[window.location.hash];
+    if (targetUrl) {
+      window.location.href = targetUrl;
+    }
+  }
+
+  // Run on load
+  window.addEventListener("load", checkHash);
+
+  // Run if hash changes
+  window.addEventListener("hashchange", checkHash);
